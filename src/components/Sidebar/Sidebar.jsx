ddf703a034b5b073f6dfdabdd797248460069152
Adelon Medeiros <adelon.medeiros@compasso.com.br>
2023-02-19 16:46:51 -0300

    feature: Ajuste do componente Avatar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "../Avatar/Avatar";
 import styles from "./Sidebar.module.css";
 export function Sidebar() {
   return (
@@ -9,10 +10,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/adelonmedeiros.png"
-        />
+        <Avatar src="https://github.com/adelonmedeiros.png" />
 
         <strong>Adelon Medeiros</strong>
         <span>QA Engineer</span>
